999d398877fa87af15cfb2e09cff0c2a773ff017
Matias Cardenas <maticardenas958@gmail.com>
2023-10-31 20:40:34 -0300

    cambios y arreglos al codigo

--- tpMemory/public/main.js
+++ tpMemory/public/main.js
@@ -2,7 +2,9 @@ const socket = new WebSocket('ws://localhost:3000');
 
 const gameBoard = document.querySelector('.memory-game');
 const scoreElements = document.querySelectorAll('.score');
-const players = {
+
+const players = 
+{
   1: { score: 0 },
   2: { score: 0 },
 };
@@ -15,8 +17,10 @@ let flippedCards = [];
 let myTurn = false;
 let canFlip = true;
 
-function createGameBoard(cards) {
-  for (let i = 0; i < cards.length; i++) {
+function createGameBoard(cards) 
+{
+  for (let i = 0; i < cards.length; i++) 
+  {
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.index = i;
@@ -24,7 +28,8 @@ function createGameBoard(cards) {
     gameBoard.appendChild(card);
 
     card.addEventListener('click', () => {
-      if (myTurn && !flippedCards.includes(i) && flippedCards.length < 2 && canFlip) {
+      if (myTurn && !flippedCards.includes(i) && flippedCards.length < 2 && canFlip) 
+      {
         flipCard(card, i);
         socket.send(JSON.stringify({ type: 'flip', index: i, card: cards[i], player: myTurn ? 1 : 2 }));
       }
@@ -32,15 +37,18 @@ function createGameBoard(cards) {
   }
 }
 
-function flipCard(card, index) {
+function flipCard(card, index) 
+{
   card.style.transform = 'rotateY(180deg)';
   card.style.backgroundImage = `url(${cards[index]})`; // Mostrar la imagen de la carta
   flippedCards.push({ card, index });
 
-  if (flippedCards.length === 2) {
-    canFlip = false; // Evitar voltear más cartas durante la comparación
+  if (flippedCards.length === 2) 
+  {
+    canFlip = false; // No voltear más cartas durante la comparación
     const [card1, card2] = flippedCards;
-    if (cards[card1.index] === cards[card2.index]) {
+    if (cards[card1.index] === cards[card2.index]) 
+    {
       setTimeout(() => {
         card1.card.classList.add('matched');
         card2.card.classList.add('matched');
@@ -48,13 +56,18 @@ function flipCard(card, index) {
         players[myTurn ? 1 : 2].score += 1;
         updateScores();
         canFlip = true; // Habilitar el volteo de cartas nuevamente
-        if (document.querySelectorAll('.card.matched').length === cards.length) {
+        if (document.querySelectorAll('.card.matched').length === cards.length) 
+        {
           // Juego terminado
           alert('¡Juego terminado! Puntuación final: YO - ' + players[1].score + ', OPONENTE - ' + players[2].score);
         }
       }, 500);
-    } else {
-      setTimeout(() => {
+    } 
+    
+    else 
+    {
+      setTimeout(() => 
+      {
         card1.card.style.transform = '';
         card1.card.style.backgroundImage = `url(${blankCardImage})`; // Revertir a la imagen de portada
         card2.card.style.transform = '';
@@ -68,27 +81,37 @@ function flipCard(card, index) {
   }
 }
 
-function updateScores() {
+function updateScores() 
+{
   scoreElements[0].textContent = `YO : ${players[1].score}`;
   scoreElements[1].textContent = `OPONENTE : ${players[2].score}`;
 }
 
-socket.onmessage = (event) => {
+socket.onmessage = (event) => 
+{
   const message = JSON.parse(event.data);
-  if (message.type === 'start') {
+  if (message.type === 'start') 
+  {
     myTurn = message.player === 1;
     cards = message.cards; // Obtener la secuencia de cartas desde el servidor
     createGameBoard(cards); // Construir el tablero de juego con las cartas
     updateScores(message.scores); // Actualizar la puntuación al inicio del juego
-  } else if (message.type === 'flip') {
+  } 
+
+  else if (message.type === 'flip') 
+  {
     flipCard(document.querySelector(`.card[data-index="${message.index}"]`), message.index);
-  } else if (message.type === 'score') {
+  } 
+
+  else if (message.type === 'score') 
+  {
     updateScores(message.scores); // Actualizar la puntuación cuando se recibe una actualización de puntuación
   }
 };
 
 
-socket.onopen = () => {
+socket.onopen = () => 
+{
   socket.send(JSON.stringify({ type: 'ready' }));
 };
 
--- tpMemory/server.js
+++ tpMemory/server.js
@@ -5,7 +5,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const players = {
+const players = 
+{
   1: { score: 0 },
   2: { score: 0 },
 };
@@ -14,8 +15,10 @@ let turn = 1; // Jugador 1 inicia
 let flippedCards = [];
 
 app.use(express.static('public'));
-// Secuencia de cartas barajada
-const images = [
+
+// Barajas las cartas
+const images = 
+[
   'images/elephant.png',
   'images/fox.png',
   'images/giraffe.png', 
@@ -23,29 +26,41 @@ const images = [
   'images/monkey.png',
   'images/snail.png'
 ];
+
 const shuffledCards = [...images, ...images].sort(() => Math.random() - 0.5);
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
+wss.on('connection', (ws) =>
+{
+  ws.on('message', (message) => 
+  {
     const data = JSON.parse(message);
 
-    if (data.type === 'ready') {
+    if (data.type === 'ready') 
+    {
       ws.send(JSON.stringify({ type: 'start', player: turn, cards: shuffledCards, scores: players }));
       turn = 3 - turn; // Alternar turno entre 1 y 2
-    } else if (data.type === 'flip') {
+    } 
+    
+    else if (data.type === 'flip') 
+    {
       const card = data.card;
       flippedCards.push(data);
 
-      if (flippedCards.length === 2) {
+      if (flippedCards.length === 2) 
+      {
         const [card1, card2] = flippedCards;
 
-        if (card1.card === card2.card) {
+        if (card1.card === card2.card) 
+        {
           players[card1.player].score += 1;
           players[card2.player].score += 1;
         }
 
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
+        wss.clients.forEach((client) => 
+        {
+
+          if (client.readyState === WebSocket.OPEN) 
+          {
             client.send(JSON.stringify({ type: 'flip', index: card1.index, card: card1.card, player: card1.player }));
             client.send(JSON.stringify({ type: 'flip', index: card2.index, card: card2.card, player: card2.player }));
             client.send(JSON.stringify({ type: 'score', scores: players }));
@@ -58,6 +73,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(3000, () => 
+{
   console.log('Servidor en ejecución en el puerto 3000');
 });
